4c1103fad0384b8ff278ae48161aebee788f1de7
Rodrigo Gomes <rslgomes@gmail.com>
2024-03-14 00:59:05 +0000

    chores(CSS): set CSS variables
    first commit, color palette and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,24 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#3a4642",
+        secondary: "#777e54",
+        dark: "#091a18",
+        light: "#fffbf4",
+        link: "#ba6748",
+        gray: {
+          50: "#f3f0e9",
+          100: "#e7e5de",
+          200: "#cecec8",
+          300: "#b6b8b2",
+          400: "#9da19b",
+          500: "#848b86",
+          600: "#6b7470",
+          700: "#535d59",
+          800: "#3a4642",
+          900: "#22302d",
+        },
       },
     },
   },
